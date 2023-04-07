c414c5791458a8193ef1b8c47f0ad45643878167
“rolee” <rolee@student.42seoul.kr>
2023-04-07 20:08:35 +0900

    Player Walk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
     void Start()
     {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // ----- Walk -----
+
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (h == 0)
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
         
+        float maxSpeed = 4.5f;
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed*(-1))
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+        
+        if (h != 0)
+            spriteRenderer.flipX = rigid.velocity.x < 0;
     }
 }
