f8ae1c17ad61643e4cdeba9c2bfa46c009804ae4
이로운 <lru040409@gmail.com>
2023-04-15 20:10:02 +0900

    create Stage4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -26,7 +26,7 @@ public class Enemy : MonoBehaviour
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
         // Platform Check
-        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.5f, rigid.position.y);
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
         if (rayHit.collider == null)
             Turn();
